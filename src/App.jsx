7645c0b47c3cb8d1128f42fefd87d2a2eca1ef79
Mikel <mickis2010.mj@gmail.com>
2025-10-27 17:32:24 +0100

    service section added

--- src/App.jsx
+++ src/App.jsx
@@ -46,8 +46,28 @@ const App = () => {
       <section className='services'>
         <div className='container'>
           <div className='services-area'>
+            <div className='service-info'>
+              <div className='service-highlight'>
+                <h4 className='service-welcome'>Our Services</h4>
+              </div>
+
+              <div className='service-title'>
+                <h3 className='service-text'>Specialized Storage for Every Special Item</h3>
+                <p className='service-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+              </div>
+            </div>
+
             <div className='services-content'>
-              <h4>Our Services</h4>
+              <div className='service-tiles'>
+                <a className='tiles' href="#">
+                  <h4 className='tile-title'>Diverse Unit Sizes</h4>
+                  <p className='tile-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                </a>
+              </div>
+
+              <div className='service-image'>
+                <img src="Bg Img.svg" alt="bg-image" />
+              </div>
             </div>
           </div>
         </div>
