393c7754b5bed65893f94705c0d6faed1603b65b
Mikel <mickis2010.mj@gmail.com>
2025-10-27 14:22:39 +0100

    navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,6 @@ import Navbar from './components/Navbar';
 const App = () => {
   return (
     <>
-      {/* <nav className='navbar'>
-        <div className='nav-container'>
-          <div className='domain'>
-            <div className='domain-contact'>
-              <p className='domain-phone'>+46 8 123 122 44</p>
-              <p className='domain-email'>contact@domain.com</p>
-            </div>
-            
-            <div className='domain-media'>
-              <img className='facebook' src="Facebook.svg" alt="facebook" />
-              <img className='twitter' src="Humble.svg" alt="twitter" />
-              <img className='instagram' src="Instagram.svg" alt="instagram" />
-              <img className='youtube' src="Youtube.svg" alt="youtube" />
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      <section className='storaid'>
-        <div className='nav-container'>
-          <div className='storaid-content'>
-            <div className='storaid-logo'>
-              <img src="Logo.svg" alt="logo" />
-            </div>
-            
-            <div className='storaid-navlinks'>
-              <a className='home' href="#">Home</a>
-              <a className='about' href="#">About Us</a>
-              <a className='service' href="#">Services</a>
-              <a className='contact' href="#">Contact Us</a>
-            </div>
-
-            <div className='storaid-button'>
-              <a className='button' href="#">Book Now</a>
-            </div>
-          </div>
-        </div>
-      </section> */}
-
       <Navbar />
 
       <section className='hero'>
@@ -96,7 +57,6 @@ const App = () => {
               </div>
 
               <div className='about-contact'>
-
                 <div className='about-button'>
                   <a className='button' href="#">Discover More</a>
                 </div>
@@ -109,12 +69,34 @@ const App = () => {
                     <h5 className='information-title'>More Information</h5>
                     <p className='information-text'>+46 8 123 122 44</p>
                   </div>
-                  
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </section>
 
+      <section className='brands'>
+        <div className='container'>
+          <div className='brands-area'>
+            <div className='brands-content'>
+              <img src="Logoipsum 1.svg" alt="brand-1" />
+              <img src="Logoipsum 2.svg" alt="brand-2" />
+              <img src="Logoipsum 3.svg" alt="brand-3" />
+              <img src="Logoipsum 4.svg" alt="brand-4" />
+              <img src="Logoipsum 5.svg" alt="brand-5" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='services'>
+        <div className='container'>
+          <div className='services-area'>
+            <div className='services-content'>
+
+            </div>
+          </div>
         </div>
       </section>
     </>
