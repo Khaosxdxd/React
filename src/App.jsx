df2597c21ab5d83bed4308dac925022acd1d40a1
Mikel <mickis2010.mj@gmail.com>
2025-10-29 21:20:10 +0100

    services/testimonials section finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import About from './components/About';
+import Brands from './components/Brands';
 
 const App = () => {
   return (
@@ -29,19 +30,7 @@ const App = () => {
 
       <About />
 
-      <section className='brands'>
-        <div className='container'>
-          <div className='brands-area'>
-            <div className='brands-content'>
-              <img src="Logoipsum 1.svg" alt="brand-1" />
-              <img src="Logoipsum 2.svg" alt="brand-2" />
-              <img src="Logoipsum 3.svg" alt="brand-3" />
-              <img src="Logoipsum 4.svg" alt="brand-4" />
-              <img src="Logoipsum 5.svg" alt="brand-5" />
-            </div>
-          </div>
-        </div>
-      </section>
+      <Brands />
 
       <section className='services'>
         <div className='container'>
@@ -58,15 +47,141 @@ const App = () => {
             </div>
 
             <div className='services-content'>
-              <div className='service-tiles'>
-                <a className='tiles' href="#">
-                  <h4 className='tile-title'>Diverse Unit Sizes</h4>
-                  <p className='tile-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
-                </a>
+              <div className='service-tiles-left'>
+                <div className='diverse'>
+                  <a className='tiles' href="#">
+                    <h4 className='tile-title'>Diverse Unit Sizes</h4>
+                    <p className='tile-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                  </a>
+                </div>
+                <div className='moving'>
+                  <a className='tiles' href="#">
+                    <h4 className='tile-title'>Moving Assistance</h4>
+                    <p className='tile-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                  </a>
+                </div>
               </div>
 
               <div className='service-image'>
                 <img src="Bg Img.svg" alt="bg-image" />
+              </div>
+              
+              <div className='service-tiles-right'>
+                <div className='vehicle'>
+                  <a className='tiles' href="#">
+                    <h4 className='tile-title'>Vehicle Storage</h4>
+                    <p className='tile-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                  </a>
+                </div>
+                
+                <div className='security'>
+                  <a className='tiles' href="#">
+                    <h4 className='tile-title'>Top-Notch Security</h4>
+                    <p className='tile-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='testimonials'>
+        <div className='container'>
+          <div className='testimonials-area'>
+            <div className='testimonials-title'>
+              <h4 className='testimonials-highlight'>Testimonials</h4>
+              <h3 className='testimonials-text'>See What Our Client Have to Say</h3>
+              <p className='testimonials-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+            </div>
+
+            <div className='testimonials-cards'>
+              <div className='testimonials-cards-area'>
+                <div className='card-left'>
+                  <div className='card-area'>
+                    <div className='card-stars'>
+                      <img src="4-Stars.svg" alt="rating" />
+                    </div>
+
+                    <div className='card-text'>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipis 
+                        cing elit. Ut libero lectus, porta nec turpis sit 
+                        amet, lobortis fringilla ante.</p>
+                    </div>
+
+                    <div className='card-info'>
+                      <div className='card-customer-info'>
+                        <img className='card-image' src="Customer-image.svg" alt="user" />
+                        <div className='card-customer-text'>
+                          <h6 className='card-name'>Aiden Harvey</h6>
+                          <p className='card-job'>Customer</p>
+                        </div>
+                        
+                      </div>
+                      <div className='card-quote'>
+                        <img className='card-quote-icon' src="Icon.svg" alt="quote" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='card-middle'>
+                  <div className='card-area'>
+                    <div className='card-stars'>
+                      <img src="4-Stars.svg" alt="rating" />
+                    </div>
+
+                    <div className='card-text'>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipis 
+                        cing elit. Ut libero lectus, porta nec turpis sit 
+                        amet, lobortis fringilla ante.</p>
+                    </div>
+
+                    <div className='card-info'>
+                      <div className='card-customer-info'>
+                        <img className='card-image' src="Customer-image.svg" alt="user" />
+                        <div className='card-customer-text'>
+                          <h6 className='card-name'>Carrisa Jocelyn</h6>
+                          <p className='card-job'>Customer</p>
+                        </div>
+                        
+                      </div>
+                      <div className='card-quote'>
+                        <img className='card-quote-icon' src="Icon.svg" alt="quote" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='card-right'>
+                  <div className='card-area'>
+                    <div className='card-stars'>
+                      <img src="4-Stars.svg" alt="rating" />
+                    </div>
+
+                    <div className='card-text'>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipis 
+                        cing elit. Ut libero lectus, porta nec turpis sit 
+                        amet, lobortis fringilla ante.</p>
+                    </div>
+
+                    <div className='card-info'>
+                      <div className='card-customer-info'>
+                        <img className='card-image' src="Customer-image.svg" alt="user" />
+                        <div className='card-customer-text'>
+                          <h6 className='card-name'>Celvin Gabriel</h6>
+                          <p className='card-job'>Customer</p>
+                        </div>
+                        
+                      </div>
+                      <div className='card-quote'>
+                        <img className='card-quote-icon' src="Icon.svg" alt="quote" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                
               </div>
             </div>
           </div>
---
+++ src/components/Brands.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Brands = () => {
+  return (
+    <>
+        <section className='brands'>
+            <div className='container'>
+                <div className='brands-area'>
+                    <div className='brands-content'>
+                        <img src="Logoipsum 1.svg" alt="brand-1" />
+                        <img src="Logoipsum 2.svg" alt="brand-2" />
+                        <img src="Logoipsum 3.svg" alt="brand-3" />
+                        <img src="Logoipsum 4.svg" alt="brand-4" />
+                        <img src="Logoipsum 5.svg" alt="brand-5" />
+                    </div>
+                </div>
+            </div>
+      </section>
+    </>
+  )
+}
+
+export default Brands
