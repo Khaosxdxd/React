e3b4d24c2aa2ec3ce7d4d8de1ee80b7ac4c560bd
Mikel <mickis2010.mj@gmail.com>
2025-10-31 06:19:48 +0100

    blog section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar';
 import About from './components/About';
 import Brands from './components/Brands';
 import WhyChooseUs from './components/WhyChooseUs';
+import LookingForSecure from './components/LookingForSecure';
 
 const App = () => {
   return (
@@ -196,59 +197,188 @@ const App = () => {
               <h4 className='pricing-highlight'>Pricing Plan</h4>
               <h3 className='pricing-title'>Find the Perfect Plan for Your Storage Needs</h3>
             </div>
-
-            <div className='pricing-images'>
-              <img src="Small Unit.svg" alt="small-unit" />
-              <img src="Medium Unit.svg" alt="medium-unit" />
-              <img src="Large Unit.svg" alt="large-unit" />
-              <img src="Executive Unit.svg" alt="executive-unit" />
-            </div>
-
-            {/* <div className='pricing-cards'>
-              <div className='pricing-cards-area'>
-                <div className='pricing-cards-content'>
-                  <div className='pricing-card-upper'>
-                    <div className='pricing-card-text'>
-                      <h5 className='price-plan'>Small Unit</h5>
-                    </div>
-                    <h3></h3>
-                    <h6></h6>
-                    <p></p>
-                  </div>
-                  <hr />
-                  <div className='pricing-card-lower'>
-                    <div className='card-list'>
-                      <ol>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                      </ol>
-                    </div>
-                    <div className='pricing-card-button'>
-
-                    </div>
+              
+            <div className='pricing-cards-content'>
+              <div className='pricing-cards'>
+                <div className='pricing-cards-area'>
+                  <div className='pricing-cards-content'>
+                    <a className='pricing-card' href="#">
+                      <div className='pricing-card-upper'>
+                        <div className='pricing-card-text'>
+                          <h5 className='price-plan'>Small Unit</h5>
+                        </div>
+                        <div className='price-cost'>
+                          <div className='price-gold'>
+                            <h6 className='price-dollar'>$</h6>
+                            <h3 className='price-amount'>50</h3>
+                          </div>
+                          <div className='price-black'>
+                            <p className='price-monthly'>/month</p>
+                          </div>
+                        </div>
+                        <div className='price-info'>
+                          <p className='price-text'>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
+                        </div>
+                        
+                      </div>
+                      <hr className='hr' />
+                      <div className='pricing-card-lower'>
+                        <div className='card-list'>
+                          <ul className='card-list-items'>
+                            <li className='card-item-1'>Nam nec ipsum in dolor</li>
+                            <li className='card-item-2'>Fusce nec ligula ut arcu</li>
+                            <li className='card-item-3'>Aliquam pulvinar arcu in</li>
+                            <li className='card-item-4'>Duis gravida enim porta</li>
+                            <li className='card-item-5'>Etiam eget libero non ligula</li>
+                          </ul>
+                        </div>
+                        <div className='pricing-card-button'>
+                          <a className='button-dark' href="#">Rent Now</a>
+                        </div>
+                      </div>
+                    </a>
+                    
                   </div>
                 </div>
               </div>
-            </div> */}
+
+              <div className='pricing-cards'>
+                <div className='pricing-cards-area'>
+                  <div className='pricing-cards-content'>
+                    <a className='pricing-card' href="#">
+                      <div className='pricing-card-upper'>
+                        <div className='pricing-card-text'>
+                          <h5 className='price-plan'>Small Unit</h5>
+                        </div>
+                        <div className='price-cost'>
+                          <div className='price-gold'>
+                            <h6 className='price-dollar'>$</h6>
+                            <h3 className='price-amount'>50</h3>
+                          </div>
+                          <div className='price-black'>
+                            <p className='price-monthly'>/month</p>
+                          </div>
+                        </div>
+                        <div className='price-info'>
+                          <p className='price-text'>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
+                        </div>
+                        
+                      </div>
+                      <hr className='hr' />
+                      <div className='pricing-card-lower'>
+                        <div className='card-list'>
+                          <ul className='card-list-items'>
+                            <li className='card-item-1'>Nam nec ipsum in dolor</li>
+                            <li className='card-item-2'>Fusce nec ligula ut arcu</li>
+                            <li className='card-item-3'>Aliquam pulvinar arcu in</li>
+                            <li className='card-item-4'>Duis gravida enim porta</li>
+                            <li className='card-item-5'>Etiam eget libero non ligula</li>
+                          </ul>
+                        </div>
+                        <div className='pricing-card-button'>
+                          <a className='button-dark' href="#">Rent Now</a>
+                        </div>
+                      </div>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+
+              <div className='pricing-cards'>
+                <div className='pricing-cards-area'>
+                  <div className='pricing-cards-content'>
+                    <a className='pricing-card' href="#">
+                      <div className='pricing-card-upper'>
+                        <div className='pricing-card-text'>
+                          <h5 className='price-plan'>Small Unit</h5>
+                        </div>
+                        <div className='price-cost'>
+                          <div className='price-gold'>
+                            <h6 className='price-dollar'>$</h6>
+                            <h3 className='price-amount'>50</h3>
+                          </div>
+                          <div className='price-black'>
+                            <p className='price-monthly'>/month</p>
+                          </div>
+                        </div>
+                        <div className='price-info'>
+                          <p className='price-text'>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
+                        </div>
+                        
+                      </div>
+                      <hr className='hr' />
+                      <div className='pricing-card-lower'>
+                        <div className='card-list'>
+                          <ul className='card-list-items'>
+                            <li className='card-item-1'>Nam nec ipsum in dolor</li>
+                            <li className='card-item-2'>Fusce nec ligula ut arcu</li>
+                            <li className='card-item-3'>Aliquam pulvinar arcu in</li>
+                            <li className='card-item-4'>Duis gravida enim porta</li>
+                            <li className='card-item-5'>Etiam eget libero non ligula</li>
+                          </ul>
+                        </div>
+                        <div className='pricing-card-button'>
+                          <a className='button-dark' href="#">Rent Now</a>
+                        </div>
+                      </div>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+
+              <div className='pricing-cards'>
+                <div className='pricing-cards-area'>
+                  <div className='pricing-cards-content'>
+                    <a className='pricing-card' href="#">
+                      <div className='pricing-card-upper'>
+                        <div className='pricing-card-text'>
+                          <h5 className='price-plan'>Small Unit</h5>
+                        </div>
+                        <div className='price-cost'>
+                          <div className='price-gold'>
+                            <h6 className='price-dollar'>$</h6>
+                            <h3 className='price-amount'>50</h3>
+                          </div>
+                          <div className='price-black'>
+                            <p className='price-monthly'>/month</p>
+                          </div>
+                        </div>
+                        <div className='price-info'>
+                          <p className='price-text'>Phasellus sollicitudin sapien eu massa accumsan, quis semper odio suscipit.</p>
+                        </div>
+                        
+                      </div>
+                      <hr className='hr' />
+                      <div className='pricing-card-lower'>
+                        <div className='card-list'>
+                          <ul className='card-list-items'>
+                            <li className='card-item-1'>Nam nec ipsum in dolor</li>
+                            <li className='card-item-2'>Fusce nec ligula ut arcu</li>
+                            <li className='card-item-3'>Aliquam pulvinar arcu in</li>
+                            <li className='card-item-4'>Duis gravida enim porta</li>
+                            <li className='card-item-5'>Etiam eget libero non ligula</li>
+                          </ul>
+                        </div>
+                        <div className='pricing-card-button'>
+                          <a className='button-dark' href="#">Rent Now</a>
+                        </div>
+                      </div>
+                    </a>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
 
-      <section className='looking-for-secure'>
-        <div className='container'>
-          <div className='lfs-area'>
-            <div className='lfs-text'>
-              <h3 className='lfs-title'>Looking for Secure and Flexible Storage? Find Your Perfect Fit With Us.</h3>
-              <p className='lfs-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
-            </div>
-            <div className='lfs-button'>
-              <a className='button-dark' href="#">Book Now</a>
-            </div>
-          </div>
-        </div>
+      <LookingForSecure />
+
+      <section className='blog'>
+
       </section>
     </>
   )
---
+++ src/components/LookingForSecure.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const LookingForSecure = () => {
+  return (
+    <>
+        <section className='looking-for-secure'>
+            <div className='container'>
+                <div className='lfs-area'>
+                    <div className='lfs-text'>
+                        <h3 className='lfs-title'>Looking for Secure and Flexible Storage? Find Your Perfect Fit With Us.</h3>
+                        <p className='lfs-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+                    </div>
+                    <div className='lfs-button'>
+                        <a className='button-dark' href="#">Book Now</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default LookingForSecure
